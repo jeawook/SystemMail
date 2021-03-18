217aac3c0495f7f650009e7c473151a433dc7611
jeawook <pjw4825@naver.com>
2021-03-18 19:03:55 +0900

    init project

---
+++ src/main/java/com/SystemMail/SystemMailApplication.java
@@ -0,0 +1,13 @@
+package com.SystemMail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemMailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemMailApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SystemMail/SystemMailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SystemMail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemMailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
