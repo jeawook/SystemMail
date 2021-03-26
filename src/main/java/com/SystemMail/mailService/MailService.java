a1d9df6e3d9df5e641e07c5ded9446c71185e8eb
jeawook <pjw4825@naver.com>
2021-03-26 12:11:43 +0900

    Create MailService.java

---
+++ src/main/java/com/SystemMail/mailService/MailService.java
@@ -0,0 +1,11 @@
+package com.SystemMail.mailService;
+
+import com.SystemMail.domain.HeaderDto;
+import com.SystemMail.domain.MailInfo;
+
+public class MailService {
+
+    public void sendMail(MailInfo mailInfo) {
+
+    }
+}
