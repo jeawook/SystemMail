1b83182b91cb397685729fd259c3d1296a054406
jeawook <pjw4825@naver.com>
2021-04-07 11:53:38 +0900

    create repository

---
+++ src/main/java/com/SystemMail/repository/ResultInfoRepository.java
@@ -0,0 +1,7 @@
+package com.SystemMail.repository;
+
+import com.SystemMail.domain.ResultInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ResultInfoRepository extends JpaRepository<ResultInfo, Long> {
+}
