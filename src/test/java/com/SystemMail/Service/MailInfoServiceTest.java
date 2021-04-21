e0ba245628b2d4627d807b1c726da019d5dd35f4
jeawook <pjw4825@naver.com>
2021-04-21 22:00:08 +0900

    Create MailInfoServiceTest.java

---
+++ src/test/java/com/SystemMail/Service/MailInfoServiceTest.java
@@ -0,0 +1,12 @@
+package com.SystemMail.Service;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class MailInfoServiceTest {
+
+
+
+}
