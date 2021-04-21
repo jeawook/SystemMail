5fb8845fac4acd1cf3cb0429d3534cc67656f01e
jeawook <pjw4825@naver.com>
2021-04-21 11:14:09 +0900

    Update MailDto.java

--- src/main/java/com/SystemMail/dto/MailDto.java
+++ src/main/java/com/SystemMail/dto/MailDto.java
@@ -2,14 +2,10 @@ package com.SystemMail.dto;
 
 import lombok.Builder;
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 
 @Data
-@Getter
-@Setter
 @Builder
-public class MailDTO {
+public class MailDto {
 
     private Long mailId;
 
