3cdc3c516aa30ac21ac162653cf04ebdf830d81a
jeawook <pjw4825@naver.com>
2021-04-26 18:07:35 +0900

    create mailgroup

---
+++ src/main/java/com/SystemMail/Service/MailGroupService.java
@@ -0,0 +1,22 @@
+package com.SystemMail.Service;
+
+import com.SystemMail.entity.MailGroup;
+import com.SystemMail.repository.MailGroupRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class MailGroupService {
+    private final MailGroupRepository mailGroupRepository;
+
+    public MailGroup createMailGroup(MailGroup mailGroup){
+        return mailGroupRepository.save(mailGroup);
+    }
+
+    public Optional<MailGroup> findMailGroup(Long id) {
+        return mailGroupRepository.findById(id);
+    }
+}
---
+++ src/main/java/com/SystemMail/repository/MailGroupRepository.java
@@ -0,0 +1,7 @@
+package com.SystemMail.repository;
+
+import com.SystemMail.entity.MailGroup;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MailGroupRepository extends JpaRepository<MailGroup, Long> {
+}
