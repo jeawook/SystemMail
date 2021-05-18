86a3789c4c78341d09603a7d75a57fde51147af2
jeawook <pjw4825@naver.com>
2021-05-18 21:02:53 +0900

    Update MailProcesser.java

---
+++ src/main/java/com/SystemMail/mail/MailProcesser.java
@@ -0,0 +1,59 @@
+package com.SystemMail.mail;
+
+import com.SystemMail.Service.SendInfoService;
+import com.SystemMail.config.DomainConnectionProperties;
+import com.SystemMail.config.MailProperties;
+import com.SystemMail.domain.entity.SendInfo;
+import com.SystemMail.domain.entity.SendStatus;
+import com.SystemMail.dto.HeaderDto;
+import com.SystemMail.dto.MailDto;
+import com.SystemMail.exception.SMTPException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.List;
+
+
+@Component
+@RequiredArgsConstructor
+public class MailProcesser {
+
+    private final MailProperties mailProperties;
+    private final SocketMailSender socketMailSender;
+    private final DomainConnectionProperties domainConnProperties;
+    private static HashMap<String, Integer> connectionInfo = new HashMap<>();
+    private static final int maxConn = 10;
+
+    public void send(List<SendInfo> sendInfoList) {
+        for (SendInfo sendInfo : sendInfoList) {
+            String domain = sendInfo.getMailGroup().getEmail().getDomain();
+            int connectionCnt = connectionInfo.getOrDefault(domain, 0);
+            int defaultMaxConn = domainConnProperties.getDomainConnectionInfo().getOrDefault("default", maxConn);
+            int maxConn = domainConnProperties.getDomainConnectionInfo().getOrDefault(domain,defaultMaxConn);
+            if (maxConn > connectionCnt) {
+                HeaderDto headerDto = HeaderDto.builder()
+                        .mailTo(sendInfo.getMailInfo().getMailTo())
+                        .mailFrom(sendInfo.getMailInfo().getMailTo())
+                        .replyTo(sendInfo.getMailInfo().getReplyTo())
+                        .contentType(mailProperties.getContentType())
+                        .encoding(mailProperties.getEncoding())
+                        .build();
+                MailDto mailDto = MailDto.builder()
+                        .headerDto(headerDto)
+                        .email(sendInfo.getMailGroup().getEmail())
+                        .content(sendInfo.makeContent())
+                        .build();
+                sendInfo.setSendStatus(SendStatus.SENDING);
+                try {
+                    socketMailSender.send(mailDto);
+                }catch (SMTPException e) {
+
+                }
+
+            }
+        }
+
+    }
+
+}
