47c3a3d2319d140b856d08f66896900233126b38
jeawook <pjw4825@naver.com>
2021-05-21 13:54:08 +0900

    Create SendInfoServiceTest.java

---
+++ src/test/java/com/SystemMail/Service/SendInfoServiceTest.java
@@ -0,0 +1,20 @@
+package com.SystemMail.Service;
+
+import com.SystemMail.repository.SendInfoRepository;
+import lombok.RequiredArgsConstructor;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@RequiredArgsConstructor
+class SendInfoServiceTest {
+
+    private SendInfoService sendInfoService;
+    private SendInfoRepository sendInfoRepository;
+
+    public void SendInfoFindTest() {
+
+    }
+
+
+}
