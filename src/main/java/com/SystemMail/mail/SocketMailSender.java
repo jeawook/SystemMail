203329b5a9aa79d679ed9885705df6f0478a02ed
jeawook <pjw4825@naver.com>
2021-05-25 16:49:42 +0900

    code update

--- src/main/java/com/SystemMail/mail/SocketMailSender.java
+++ src/main/java/com/SystemMail/mail/SocketMailSender.java
@@ -1,6 +1,7 @@
 package com.SystemMail.mail;
 
 import com.SystemMail.Service.SendInfoService;
+import com.SystemMail.common.SmtpCode;
 import com.SystemMail.domain.entity.Email;
 import com.SystemMail.domain.entity.SendInfo;
 import com.SystemMail.dto.MailDto;
@@ -24,6 +25,7 @@ public class SocketMailSender{
     private String serverReply;
     static private final String serverDomain = "sender.com";
     static private final int PORT = 25;
+
     private final SendInfoService sendInfoService;
 
 
@@ -32,7 +34,7 @@ public class SocketMailSender{
      * @param mailDto 메일 발송 정보
      * @throws SMTPException
      */
-    //@Async("threadPoolTaskExecutor")
+    @Async("threadPoolTaskExecutor")
     public void send(MailDto mailDto) throws SMTPException{
         connect(mailDto.getEmail().getDomain());
         hail(mailDto.getHeaderDto().getMailTo(), mailDto.getHeaderDto().getMailFrom());
@@ -55,7 +57,7 @@ public class SocketMailSender{
             input = new BufferedReader(new InputStreamReader(smtp.getInputStream()));
             output = new PrintStream(smtp.getOutputStream());
             serverReply = input.readLine();
-            if(serverReply.startsWith("250") || serverReply.startsWith("354")|| serverReply.startsWith("221")){
+            if(serverReply.startsWith(SmtpCode.SUCCESS) || serverReply.startsWith(SmtpCode.PROCESS)){
 
             }else{
                 throw new SMTPException("Error connecting to SMTP server " + lookup+" on port "+PORT);
@@ -67,11 +69,11 @@ public class SocketMailSender{
     }
 
     private void hail(Email mailFrom, Email mailTo) throws SMTPException{
-        if(submitCommand(SMTPCommand.HELO + serverDomain))
+        if(submitCommand(SMTPCommand.HELO + serverDomain).startsWith(SmtpCode.SUCCESS))
             throw new SMTPException("Error occured during HELO command.");
-        if(submitCommand(SMTPCommand.create(SMTPCommand.MAILFROM,mailFrom.getAddress())))
+        if(submitCommand(SMTPCommand.create(SMTPCommand.MAILFROM,mailFrom.getAddress())).startsWith(SmtpCode.SUCCESS))
             throw new SMTPException("Error during MAIL command");
-        if(submitCommand(SMTPCommand.create(SMTPCommand.RCPTTO,mailTo.getAddress())))
+        if(submitCommand(SMTPCommand.create(SMTPCommand.RCPTTO,mailTo.getAddress())).startsWith(SmtpCode.SUCCESS))
             throw new SMTPException("Error during RCPT command.");
     }
 
@@ -84,10 +86,10 @@ public class SocketMailSender{
     private void sendMessage(MailDto mailDto) throws SMTPException{
         StringBuilder sb = new StringBuilder();
         try{
-            if(submitCommand(SMTPCommand.DATA))
+            if(submitCommand(SMTPCommand.DATA).startsWith(SmtpCode.PROCESS))
                 throw new SMTPException("Error during DATA command.");
                 sb.append(mailDto.getHeaderDto().getHeaderInfo());
-            if(submitCommand(sb.toString()+mailDto.getContent()+"\r\n."))
+            if(submitCommand(sb.toString()+mailDto.getContent()+"\r\n.").startsWith(SmtpCode.SUCCESS))
                 throw new SMTPException("Error during mail transmission.");
         }catch(Exception e){
         }
@@ -99,13 +101,11 @@ public class SocketMailSender{
      * @return
      * @throws SMTPException
      */
-    private boolean submitCommand(String command) throws SMTPException{
+    private String submitCommand(String command) throws SMTPException{
         try{
             output.print(command+"\r\n");
             serverReply = input.readLine();
-            if(serverReply.charAt(0)=='4'||serverReply.charAt(0)=='5')
-                return true;
-            else return false;
+            return serverReply;
         }catch(Exception e){
             throw new SMTPException(e.getMessage());
         }
@@ -117,7 +117,7 @@ public class SocketMailSender{
      */
     private void quit() throws SMTPException {
         try{
-            if(submitCommand("Quit"))
+            if(submitCommand("Quit").startsWith(SmtpCode.SERVER_CLOSE))
                 throw new SMTPException("Error during QUIT command");
             input.close();
             output.flush();
