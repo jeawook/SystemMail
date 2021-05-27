7a223e7dd5ef1ad6038e0d556d333b11c71b94df
jeawook <pjw4825@naver.com>
2021-05-27 20:44:26 +0900

    Update MailResultInfoRepository.java

--- src/main/java/com/SystemMail/repository/MailResultInfoRepository.java
+++ src/main/java/com/SystemMail/repository/MailResultInfoRepository.java
@@ -3,5 +3,5 @@ package com.SystemMail.repository;
 import com.SystemMail.domain.entity.MailResultInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ResultInfoRepository extends JpaRepository<MailResultInfo, Long> {
+public interface MailResultInfoRepository extends JpaRepository<MailResultInfo, Long> {
 }
