d99ded3830750e904f10194405eaa03336cfe93f
jeawook <pjw4825@naver.com>
2021-05-27 20:44:32 +0900

    Create MailResultDetailRepository.java

---
+++ src/main/java/com/SystemMail/repository/MailResultDetailRepository.java
@@ -0,0 +1,7 @@
+package com.SystemMail.repository;
+
+import com.SystemMail.domain.entity.MailResultDetail;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MailResultDetailRepository extends JpaRepository<MailResultDetail, Long> {
+}
