68d8255aae13592c1a15faa0a80e8ac4f60b4edb
jeawook <pjw4825@naver.com>
2021-05-27 20:44:45 +0900

    delete

--- src/main/java/com/SystemMail/repository/ResultDetailRepository.java
+++
@@ -1,7 +0,0 @@
-package com.SystemMail.repository;
-
-import com.SystemMail.domain.entity.MailResultDetail;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ResultDetailRepository extends JpaRepository<MailResultDetail, Long> {
-}
