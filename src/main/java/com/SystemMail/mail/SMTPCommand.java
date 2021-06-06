f28eba615f8ae947a19ea5b7685ff9968f6ad309
jeawook <pjw4825@naver.com>
2021-06-06 18:53:05 +0900

    add smtp command

--- src/main/java/com/SystemMail/mail/SMTPCommand.java
+++ src/main/java/com/SystemMail/mail/SMTPCommand.java
@@ -6,6 +6,7 @@ public class SMTPCommand {
     public static final String MAILFROM = "MAIL FROM:";
     public static final String RCPTTO = "RCPT TO:";
     public static final String DATA = "DATA";
+    public static final String QUIT = "QUIT";
 
     public static String create(String key, String value) {
         return String.format("%s: %s", key, value);
